88633a84cc2fbb54c25797fc75bc238da64c940c
helena-go <helena.g.o1206@gmail.com>
2024-11-07 13:41:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 
+# Cargar el archivo CSS desde el repositorio de GitHub (reemplaza con la URL de tu archivo CSS)
+st.markdown(
+    """
+    <style>
+    @import url('https://raw.githubusercontent.com/usuario/repositorio/main/styles.css');
+    </style>
+    """, 
+    unsafe_allow_html=True
+)
+
+# Aquí va el resto de tu aplicación Streamlit
+st.title("Análisis de Rentabilidad en la expansión hotelera.")
+st.header("Eurostars Hotel Company.")
+st.write("Visualización de los datos.")
