c238d1d897195ac591ac607434acafb7091f5188
helena-go <helena.g.o1206@gmail.com>
2024-11-07 13:34:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ st.set_page_config(
 
 # Título principal de la aplicación en la página
 st.title("Análisis de Rentabilidad Hotelera 🏨")
+
+#COLOR DE TEXTO
+
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1 style="color: #2E86C1;">Análisis de Rentabilidad Hotelera 🏨</h1>
+        <p style="font-size:18px; color: #34495E;">Explora la rentabilidad de ubicaciones para la expansión hotelera con visualizaciones interactivas</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
