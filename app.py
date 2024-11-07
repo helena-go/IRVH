38cae9fa016d5508dae9eaf6f34dc3e4fb85b1b3
helena-go <helena.g.o1206@gmail.com>
2024-11-07 13:31:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.header("Hola peques")
+st.set_page_config(
+    page_title="Análisis de Rentabilidad Hotelera",  # Título en la pestaña
+    page_icon="🏨",  # Ícono en la pestaña, puedes usar emojis o enlaces de imágenes
+    layout="wide"  # Configuración de ancho de página, puede ser "centered" o "wide"
+)
+
+# Título principal de la aplicación en la página
+st.title("🔍 Análisis de Rentabilidad Hotelera 🏨")
