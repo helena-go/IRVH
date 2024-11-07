5d78ba28bd971b66ae77b0e049443ede501adbed
helena-go <helena.g.o1206@gmail.com>
2024-11-07 13:48:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import streamlit as st
 
 
-# Cargar el archivo CSS desde el repositorio de GitHub (reemplaza con la URL de tu archivo CSS)
-st.markdown(
-    """
-    <style>
-    @import url('https://github.com/helena-go/IRVH/blob/88633a84cc2fbb54c25797fc75bc238da64c940c/styles.css');
-    </style>
-    """, 
-    unsafe_allow_html=True
+st.set_page_config(
+    page_title="Análisis de Rentabilidad Hotelera",  # Título en la pestaña
+    page_icon="🏨",  # Ícono en la pestaña, puedes usar emojis o enlaces de imágenes
+    layout="wide"  # Configuración de ancho de página, puede ser "centered" o "wide"
 )
 
+# Título principal de la aplicación en la página
+st.title("Análisis de Rentabilidad Hotelera 🏨")
+
+#COLOR DE TEXTO
+
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <h1 style="color: #2E86C1;">Análisis de Rentabilidad Hotelera 🏨</h1>
+        <p style="font-size:18px; color: #34495E;">Explora la rentabilidad de ubicaciones para la expansión hotelera con visualizaciones interactivas</p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
